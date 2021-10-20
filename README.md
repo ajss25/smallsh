cddc1a28c98418eaa2ef6539896e5785676f6a04
Alex Shin <shinal@os1.engr.oregonstate.edu>
2021-10-20 02:25:45 -0700

    Get user command and parse arguments

---
+++ README.md
@@ -0,0 +1,4 @@
+How to compile smallsh w/ gcc:
+
+gcc --std=gnu99 -o smallsh smallsh.c
+./smallsh
---
+++ smallsh.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+// shell supports command line inputs of up to 2048 characters and 512 arguments
+#define MAX_COMMAND_LENGTH 2048
+#define MAX_COMMAND_ARGS 512
+
+int main(void) {
+	char command[MAX_COMMAND_LENGTH];
+	char* args[MAX_COMMAND_ARGS];
+
+	// prompt command line input, flush output buffer, get command line input
+	printf(": ");
+	fflush(stdout);
+	fgets(command, MAX_COMMAND_LENGTH, stdin);
+
+	// parse command line input into separate arguments
+	int i = 0;
+	char *token = strtok(command, " \n");
+
+	while (token) {
+		args[i] = token;
+		token = strtok(NULL, " \n");
+		i++; 
+	}
+}
