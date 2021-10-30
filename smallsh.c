9c02a8a907ce923fdbb77d0447133f2dd6f84d45
Alex Shin <shinal@os1.engr.oregonstate.edu>
2021-10-29 22:04:14 -0700

    Add/update descriptive comments for readability

--- smallsh.c
+++ smallsh.c
@@ -7,7 +7,7 @@
 #include <fcntl.h>
 #include <stdbool.h>
 
-// shell supports command line inputs of up to 2048 characters and 512 arguments
+// smallsh supports commands of up to 2048 characters and 512 arguments
 #define MAX_COMMAND_LENGTH 2048
 #define MAX_COMMAND_ARGS 512
 
@@ -18,13 +18,18 @@ int childProcessCount = 0;
 int childProcessPids[80];
 bool foregroundOnlyMode = false;
 
-// initialize empty struct for SIGINT and SIGTSTP
+// initialize empty structs for SIGINT and SIGTSTP
+// reference: Exploration: Signal Handling API
 struct sigaction SIGINT_action = {0};
 struct sigaction SIGTSTP_action = {0};
 
-// function to handle SIGTSTP
+/*
+ * ctrl-z command will enter/exit shell into/from foreground-only mode
+ * display informative message and prompt user to enter a command
+ * reference: Exploration: Signal Handling API
+ */
 void handle_SIGTSTP() {
-	// enter foreground-only mode
+	// if shell is not in foreground-only mode, enter foreground-only mode
 	if (!foregroundOnlyMode) {
 		foregroundOnlyMode = true;
 
@@ -35,7 +40,7 @@ void handle_SIGTSTP() {
 		char* rePrompt = "\n: ";
 		write(STDOUT_FILENO, rePrompt, 3);
 		fflush(stdout);
-	// exit foreground-only mode
+	// else, exit foreground-only mode
 	} else {
 		foregroundOnlyMode = false;
 
@@ -49,8 +54,11 @@ void handle_SIGTSTP() {
 	}
 }
 
-// function to print exit status or terminating signal
-// of the last foreground process ran by the shell
+/*
+ * 'status' command
+ * print exit status or the terminating signal of the last foreground process ran by the shell
+ * reference: Exploration: Process API - Monitoring Child Processes
+ */
 void printStatus(int status) {
 	if (WIFEXITED(status)) {
 		printf("exit value %d\n", WEXITSTATUS(status));
@@ -60,53 +68,66 @@ void printStatus(int status) {
 	fflush(stdout);
 }
 
-// function to check status of child processes
+/*
+ * check the status of background child processes
+ * display informative message upon finding a process that completed
+ */
 void checkChildProcesses(void) {
+	// initialize childExitMethod with bogus value
 	int childExitMethod = -5;
-	int i;
 	
-	// loop through child process pids to check if finished running
+	// check child process pids to check if any completed
+	int i;
 	for (i = 0; i < childProcessCount; i++) {
 		if (childProcessPids[i] == -5) {
 			continue;
 		}
 		else if (waitpid(childProcessPids[i], &childExitMethod, WNOHANG)) {
-				// print results
+				// display message when a child process completed
 				printf("background pid %d is done: ", childProcessPids[i]);
 				printStatus(childExitMethod);
 				fflush(stdout);
+				// set completed process' pid to a bogus value to no longer check
 				childProcessPids[i] = -5;
 		}
 	}
 }
 
-// function to execute commands in the foreground
+/*
+ * function to execute commands in the foreground
+ * check for I/O redirection and fork a child process
+ * references: 
+ * Explorations: Processes and I/O, Process API - Creating and Terminating Processes, Signal Handling API
+ */
 void executeFgCommands(char**args, int argsCount) {
-	// check for I/O redirection
+	// save stdin/stdout to re-open them after completion of command with redirection
 	// reference: https://stackoverflow.com/questions/9084099/re-opening-stdout-and-stdin-file-descriptors-after-closing-them
 	int saveStdin = dup(0);
 	int saveStdout = dup(1);
 
-	// iterate over command to check for redirection and set file descriptors
 	int sourceFD = -1;
 	int targetFD = -1;
-	int redirection = 0;
+	bool redirection = false;
 
+	// iterate over entered command to check for redirection and set file descriptors
 	int i;
 	for (i = 0; i < argsCount; i++) {
+		// if command contains "<", mark redirection as true and open input file
 		if (strcmp(args[i], "<") == 0) {
-			redirection = 1;
+			redirection = true;
 			sourceFD = open(args[i+1], O_RDONLY);
+			// display error if file cannot be opened for input and set status to 1
 			if (sourceFD == -1) {
 				printf("cannot open %s for input\n", args[i+1]);
 				fflush(stdout);
 				status = 1;
 				return;
 			}
-		
+		// if command contains ">", mark redirection as true and open output file
 		} else if (strcmp(args[i], ">") == 0) {
-			redirection = 1;
+			redirection = true;
 			targetFD = open(args[i+1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			// display error if file cannot be opened for output and set status to 1
 			if (targetFD == -1) {
 				printf("cannot open %s for output\n", args[i+1]);
 				fflush(stdout);
@@ -115,8 +136,8 @@ void executeFgCommands(char**args, int argsCount) {
 			}
 		}
 	}
-
-	if (redirection == 1) {
+	// if redirection in command, close stdin/stdout depending on file descriptors being opened
+	if (redirection) {
 		if (sourceFD != -1 && targetFD != -1) {
 			close(0);
 			close(1);
@@ -126,7 +147,7 @@ void executeFgCommands(char**args, int argsCount) {
 			close(1);
 		}
 		
-		// if source file descriptor was found, dup2
+		// if source file descriptor was found, use dup2 for redirection
 		if (sourceFD != -1) {
 			int sourceResult = dup2(sourceFD, 0);
 			if (sourceResult == -1) {
@@ -135,7 +156,7 @@ void executeFgCommands(char**args, int argsCount) {
 			}
 		}
 
-		// if target file descriptor was found, dup2
+		// if target file descriptor was found, use dup2 for redirection
 		if (targetFD != -1) {
 			int targetResult = dup2(targetFD, 1);
 			if (targetResult == -1) {
@@ -144,20 +165,18 @@ void executeFgCommands(char**args, int argsCount) {
 			}
 		}
 
-		// after using dup2() to set up redirection, 
-		// nullify redirection symbols and the destination/source from the command 
-		// to pass just the command into exec()
+		// nullify redirection symbols, source, and destination from the command
 		int j;
 		for (j = 1; j < argsCount; j++) {
 			args[j] = NULL;
 		}
-
+		// close file descriptors
 		close(sourceFD);
 		close(targetFD);
 	} 
 
+	// if there is no redirection in the command, set last argument as NULL for execvp()
 	else {
-		// set last argument to NULL for execvp()
 		if (background && foregroundOnlyMode) {
 			args[argsCount - 1] = NULL;
 		} else {
@@ -165,7 +184,7 @@ void executeFgCommands(char**args, int argsCount) {
 		}
 	}
 	
-	// initialize with bogus values
+	// initialize pid and exit method of child process with bogus values
 	pid_t spawnPid = -5; 
 	int childExitMethod = -5;
 
@@ -173,9 +192,9 @@ void executeFgCommands(char**args, int argsCount) {
 	spawnPid = fork();
 
 	switch (spawnPid) {
-		// error
+		// if error, display error and exit with a status of 1
 		case -1:
-			perror("Error\n");
+			perror("fork() error\n");
 			exit(1);
 			break;
 
@@ -185,8 +204,9 @@ void executeFgCommands(char**args, int argsCount) {
 			SIGINT_action.sa_handler = SIG_DFL;
 			sigaction(SIGINT, &SIGINT_action, NULL);
 
+			// execute the command
 			execvp(args[0], args);
-			// return error and set value retrieved by built-in `status` command to 1
+			// if error, display error and exit with a status of 1
 			perror(args[0]);
 			exit(1);
 			break;
@@ -194,7 +214,7 @@ void executeFgCommands(char**args, int argsCount) {
 		// parent process
 		default:
 			waitpid(spawnPid, &childExitMethod, 0);
-			// print and set status accordingly
+			// print and set status of the child process
 			if (WIFSIGNALED(childExitMethod)) {
 				status = childExitMethod;
 				printStatus(childExitMethod);
@@ -202,7 +222,8 @@ void executeFgCommands(char**args, int argsCount) {
 				status = childExitMethod;
 			}
 	}
-	if (redirection == 1) {
+	// if redirection in the command, re-open stdin/stdout
+	if (redirection) {
 		dup2(saveStdin, 0);
 		dup2(saveStdout, 1);
 		close(saveStdin);
@@ -210,33 +231,40 @@ void executeFgCommands(char**args, int argsCount) {
 	}
 }
 
-// function to execute commands in the background
+/*
+ * function to execute commands in the background
+ * check for I/O redirection and fork a child process
+ * reference: Explorations: Processes and I/O, Process API - Creating and Terminating Processes
+ */
 void executeBgCommands(char** args, int argsCount) {	
-	// check for I/O redirection
+	// save stdin/stdout to re-open them after completion of command with redirection
 	// reference: https://stackoverflow.com/questions/9084099/re-opening-stdout-and-stdin-file-descriptors-after-closing-them
 	int saveStdin = dup(0);
 	int saveStdout = dup(1);
 
-	// iterate over command to check for redirection and set file descriptors
 	int sourceFD = -1;
 	int targetFD = -1;
-	int redirection = 0;
+	int redirection = false;
 
+	// iterate over command to check for redirection and set file descriptors
 	int i;
 	for (i = 0; i < argsCount; i++) {
 		if (strcmp(args[i], "<") == 0) {
-			redirection = 1;
+			// if command contains "<", mark redirection as true and open input file
+			redirection = true;
 			sourceFD = open(args[i+1], O_RDONLY);
+			// display error if file cannot be opened for input and set status to 1
 			if (sourceFD == -1) {
 				printf("cannot open %s for input\n", args[i+1]);
 				fflush(stdout);
 				status = 1;
 				return;
 			}
-		
+		// if command contains ">", mark redirection as true and open output file
 		} else if (strcmp(args[i], ">") == 0) {
-			redirection = 1;
+			redirection = true;
 			targetFD = open(args[i+1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			// display error if file cannot be opened for output and set status to 1
 			if (targetFD == -1) {
 				printf("cannot open %s for output\n", args[i+1]);
 				fflush(stdout);
@@ -246,50 +274,49 @@ void executeBgCommands(char** args, int argsCount) {
 		}
 	}
 
-	// if redirection
-	if (redirection == 1) {
+	// if redirection in command, close stdin/stdout
+	if (redirection) {
 		close(0);
 		close(1);
 
-		// if source file descriptor was found, dup2
+		// if source file descriptor was found, dup2 for redirection
 		int sourceResult;
 		if (sourceFD != -1) {
 			sourceResult = dup2(sourceFD, 0);
-		// else stdin to /dev/null
+		// else, set stdin to /dev/null
 		} else {
 			sourceResult = open("/dev/null", O_RDONLY);
 		}
-
+		// check for errors using dup2
 		if (sourceResult == -1) {
 			perror("source dup2()");
 			status = 2;
 		}
 
-		// if target file descriptor was found, dup2
+		// if target file descriptor was found, dup2 for redirection
 		int targetResult;
 		if (targetFD != -1) {
 			targetResult = dup2(targetFD, 1);
-		// else stdout to /dev/null
+		// else, set stdout to /dev/null
 		} else {
 			targetResult = open("/dev/null", O_WRONLY | O_CREAT | O_TRUNC, 0644);
 		}
-
+		// check for errors using dup2
 		if (targetResult == -1) {
 			perror("target dup2()");
 			status = 2;
 		}
-		// after using dup2() to set up redirection, 
-		// nullify redirection symbols and the destination/source from the command 
-		// to pass just the command into exec()
+		// nullify redirection symbols, source, and destination from the command
 		int j;
 		for (j = 1; j < argsCount; j++) {
 			args[j] = NULL;
 		}
+		// close file descriptors
 		close(sourceFD);
 		close(targetFD);
 	}
 
-	// if no redirection, set stdin/stdout to /dev/null
+	// if there is no redirection in the command, set stdin/stdout to /dev/null
 	else {
 		int sourceResult = open("/dev/null", O_RDONLY);
 		if (sourceResult == -1) {
@@ -308,7 +335,7 @@ void executeBgCommands(char** args, int argsCount) {
 		// replace "&" to set last argument to NULL for execvp()
 		args[argsCount - 1] = NULL;
 	}
-	// initialize with bogus values
+	// initialize pid and exit method of child process with bogus values
 	pid_t spawnPid = -5;
 	int childExitMethod = -5;
 
@@ -316,29 +343,34 @@ void executeBgCommands(char** args, int argsCount) {
 	spawnPid = fork();
 
 	switch (spawnPid) {
-		// error
+		// if error, display error and exit with a status of 1
 		case -1:
-			perror("Error\n");
+			perror("fork() error\n");
 			exit(1);
 			break;
 
 		// child process
 		case 0:
+			// execute the command
 			execvp(args[0], args);
-			// return error and set value retrieved by built-in `status` command to 1
+			// if error, display error and exit with a status of 1
 			perror(args[0]);
 			exit(1);
 			break;
 
+		// parent process
 		default:
 			waitpid(spawnPid, &childExitMethod, WNOHANG);
+			// display message about child process running in the background
 			printf("background pid is %d\n", spawnPid);
 			fflush(stdout);
-
+			// set background flag back to false for subsequent commands
 			background = false;
+			// add forked child process pid to array to check its status
 			childProcessPids[childProcessCount] = spawnPid;
 			childProcessCount++;
 	}
+	// re-open stdin/stdout
 	close(0);
 	close(1);
 	dup2(saveStdin, 0);
@@ -347,8 +379,11 @@ void executeBgCommands(char** args, int argsCount) {
 	close(saveStdout);
 }
 
-// function to kill process and jobs, and exit the shell
-// reference: https://www.ibm.com/docs/en/zos/2.4.0?topic=functions-kill-send-signal-process
+/*
+ * 'exit' command
+ * function to kill process and jobs and exit the shell
+ * reference: https://www.ibm.com/docs/en/zos/2.4.0?topic=functions-kill-send-signal-process
+ */
 void exitShell(void) {
 	int i;
 	for (i = 0; i < childProcessCount; i++) {
@@ -357,8 +392,11 @@ void exitShell(void) {
 	exit(0);
 }
 
-// function to change to given directory
-// change to home directory if not given the destination
+/*
+ * 'cd' command
+ * function to change directory
+ * change to home directory if 'cd' was not given a destination
+ */
 void changeDirectory(char *directory) {
 	int dirChanged;
 	if (strcmp(directory, "HOME") == 0) {
@@ -368,14 +406,19 @@ void changeDirectory(char *directory) {
 	}
 }
 
-// function to get and parse command line input from user
+/*
+ * function to get and parse command from user
+ * prompt user for command and parse command for variable expansion/comments
+ * additional checks for running command in the background, and built-in commands
+ */
+
 int getAndParseCommand(void) {
 	char command[MAX_COMMAND_LENGTH];
 	char* args[MAX_COMMAND_ARGS];
 	int argsCount = 0;
 	pid_t pid = getpid();
 
-	// prompt command line input, flush output buffer, get command line input
+	// prompt and get command
 	printf(": ");
 	fflush(stdout);
 	fgets(command, MAX_COMMAND_LENGTH, stdin);
@@ -388,7 +431,7 @@ int getAndParseCommand(void) {
 
 	// parse command line input into separate arguments
 	while (token) {
-		// replace substring of "$$" at the end of any argument with pid of the shell
+		// replace substring of "$$" within the command with pid of the shell
 		// reference: https://stackoverflow.com/questions/12784766/check-substring-exists-in-a-string-in-c
 		if (strstr(token, "$$") != NULL) {
 			int j;
@@ -398,7 +441,7 @@ int getAndParseCommand(void) {
 					// reference: https://stackoverflow.com/questions/20342559/how-to-cut-part-of-a-string-in-c
 					token[j] = '\0';
 
-					// create a new argument string
+					// create a new argument string to expand the $$ variable and store the argument
 					char newArg[sizeof(token) - 2 + sizeof(pid)];
 					sprintf(newArg, "%s%d", token, pid);
 					args[i] = newArg;
@@ -412,7 +455,7 @@ int getAndParseCommand(void) {
 		argsCount++;
 	}
 
-	// check if command needs to be executed in the background
+	// check and flag if command is to be executed in the background
 	if (strcmp(args[argsCount - 1], "&") == 0) {
 		background = true;
 	} else {
@@ -422,16 +465,16 @@ int getAndParseCommand(void) {
 	// re-prompt for another command if input is a comment or a blank line
 	if (args[0][0] == '#' || args[0][0] == '\n') {
 		return 0;
-	// if `exit` command was given, kill processes/jobs and exit the shell
-	// execute this command in foreground only
+
+	// if 'exit' command was given, kill processes/jobs and exit the shell
 	} else if (strcmp(args[0], "exit") == 0) {
+		// execute this command in foreground only
 		background = false;
 		exitShell();
 
-	// if `cd` command was given, change the working directory
-	// if the command was not given an argument, change to home directory
-	// execute this command in foreground only
+	// if 'cd' command was given, change the working directory
 	} else if (strcmp(args[0], "cd") == 0) {
+		// execute this command in foreground only
 		background = false;
 		if (argsCount == 1 || strcmp(args[1], "&") == 0) {
 			char dir[] = "HOME";
@@ -440,13 +483,13 @@ int getAndParseCommand(void) {
 			changeDirectory(args[1]);
 		}
 
-	// if `status` command was given, print exit status or terminating signal
-	// execute this command in foreground only
+	// if 'status' command was given, print exit status or terminating signal of the last foreground process ran by the shell
 	} else if (strcmp(args[0], "status") == 0) {
+		// execute this command in foreground only
 		background = false;
 		printStatus(status);
 
-	// if we need to execute any other commands
+	// execute any other commands
 	} else {
 		if (!background || foregroundOnlyMode) {
 			executeFgCommands(args, argsCount);
@@ -457,6 +500,11 @@ int getAndParseCommand(void) {
 	return 0;
 }
 
+/*
+ * main function to register signal handles and
+ * continuously prompt user for command until shell exits
+ * reference: Exploration: Signal Handling API
+ */
 int main(void) {
 	// register SIG_IGN as signal handler for SIGINT_action struct
 	SIGINT_action.sa_handler = SIG_IGN;
@@ -467,14 +515,15 @@ int main(void) {
 	SIGTSTP_action.sa_handler = handle_SIGTSTP;
 	// block catchable signals
 	sigfillset(&SIGTSTP_action.sa_mask);
-	// use flag to restart after signal handler is done
+
+	// use flag to restart after signal handler is done and 
 	SIGTSTP_action.sa_flags = SA_RESTART;
 	// register SIGTSTP_action as handler for SIGTSTP to handle foreground modes
 	sigaction(SIGTSTP, &SIGTSTP_action, NULL);
 
-	// prompt user for command line input until exited
+	// continuously run the shell for command line input until exited
 	while (1) {
-		// check status of background child processes
+		// before getting and parsing command, check status of background child processes each time
 		checkChildProcesses();
 		getAndParseCommand();
 	}
